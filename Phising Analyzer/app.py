fde8deb85663e8eb57d44f14adf5eb1fcd8f77ad
mammadlilqar <116678497+mammadlilqar@users.noreply.github.com>
2023-12-14 20:21:34 +0400

    Add files via upload

---
+++ Phising Analyzer/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+# Replace 'YOUR_VIRUSTOTAL_API_KEY' with your actual VirusTotal API key
+virustotal_api_key = '49127fa6f24f5004aa2b78f6cf65bba746ad482db76712879f1481506aa461fb'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/check_ip', methods=['POST'])
+def check_ip():
+    try:
+        ip = request.json.get('ip')
+
+        if not ip:
+            raise ValueError('IP address not provided in the request.')
+
+        virustotal_url = f'https://www.virustotal.com/api/v3/ip_addresses/{ip}'
+        headers = {'x-apikey': virustotal_api_key}
+
+        response = requests.get(virustotal_url, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if 'data' in data and 'attributes' in data['data']:
+            attributes = data['data']['attributes']
+            
+            last_analysis_stats = attributes.get('last_analysis_stats', {})
+            malicious_count = last_analysis_stats.get('malicious', 0)
+            suspicious_count = last_analysis_stats.get('suspicious', 0)
+
+            last_analysis_results = attributes.get('last_analysis_results', {})
+            scan_date = last_analysis_results.get('scan_date', 'N/A')
+            scan_results = last_analysis_results.get('result', 'N/A')
+
+            country = attributes.get('country', 'N/A')
+            
+            return jsonify({
+                'data': {
+                    'abuseConfidenceScore': malicious_count,
+                    'suspiciousConfidenceScore': suspicious_count,
+                    'numReports': malicious_count + suspicious_count,
+                    'scanDate': scan_date,
+                    'scanResults': scan_results,
+                    'country': country,
+                }
+            })
+        else:
+            return jsonify({'error': 'No information available for this IP on VirusTotal'})
+
+    except requests.RequestException as e:
+        return jsonify({'error': f'Error connecting to VirusTotal API: {str(e)}'})
+    except Exception as e:
+        return jsonify({'error': f'Unexpected error: {str(e)}'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Phising Analyzer/static/script.js
@@ -0,0 +1,110 @@
+function handleFileSelect() {
+    var fileInput = document.getElementById("file-input");
+    var statusElement = document.getElementById("status");
+    var headerElement = document.getElementById("email-header");
+    var bodyElement = document.getElementById("email-body");
+    var senderIpElement = document.getElementById("sender-ip");
+    var abuseInfoElement = document.getElementById("abuse-info");
+
+    if (fileInput.files.length > 0) {
+        var file = fileInput.files[0];
+
+        if (file.name.endsWith(".eml")) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                var content = e.target.result;
+
+                // Parse headers and body
+                var headerEnd = content.indexOf("\r\n\r\n");
+                if (headerEnd !== -1) {
+                    var headers = content.substring(0, headerEnd);
+                    var body = content.substring(headerEnd + 4); // Skip the double line break
+
+                    // Extract sender's IP address using a more permissive regular expression
+                    var match = headers.match(/Received:.*\[(\S+)\]/);
+                    var senderIp = match ? match[1] : "IP not found";
+
+                    // Display the content of the .eml file
+                    headerElement.textContent = `Email Headers:\n${headers}`;
+                    bodyElement.textContent = `Email Body:\n${body}`;
+                    senderIpElement.innerHTML = `Sender's IP: ${senderIp}`;
+
+                    // Check if the IP is malicious using the VirusTotal API
+                    console.log("Checking IP for abuse:", senderIp);
+                    checkIPForAbuse(senderIp);
+                    statusElement.textContent = ""; // Clear any previous status messages
+                } else {
+                    // Handle case where headers are not found
+                    headerElement.textContent = "Invalid .eml file. Headers not found.";
+                    bodyElement.textContent = "";
+                    senderIpElement.textContent = "";
+                    abuseInfoElement.textContent = ""; // Clear abuse info
+                    statusElement.textContent = "";
+                }
+            };
+
+            reader.readAsText(file);
+            statusElement.textContent = "Selected file: " + file.name;
+        } else {
+            statusElement.textContent = "Please select a valid .eml file.";
+            // Clear the file input
+            fileInput.value = "";
+            headerElement.textContent = "";
+            bodyElement.textContent = "";
+            senderIpElement.textContent = "";
+            abuseInfoElement.textContent = ""; // Clear abuse info
+        }
+    } else {
+        statusElement.textContent = "Please select a file.";
+    }
+}
+
+function checkIPForAbuse(ip) {
+    console.log("Checking IP for abuse:", ip);
+
+    fetch('/check_ip', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ip: ip }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("VirusTotal response:", data);
+            // Display the VirusTotal information
+            displayAbuseInfo(data, ip);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+function displayAbuseInfo(abuseData, ip) {
+    var senderIpElement = document.getElementById("sender-ip");
+    var abuseInfoElement = document.getElementById("abuse-info");
+
+    senderIpElement.innerHTML = `Sender's IP: ${ip}`;
+
+    console.log("Displaying Abuse Info:", abuseData); // Log the received data for debugging
+
+    if (abuseData && abuseData.data) {
+        var abuseConfidenceScore = abuseData.data.abuseConfidenceScore || 0;
+        var suspiciousConfidenceScore = abuseData.data.suspiciousConfidenceScore || 0;
+        var numReports = abuseData.data.numReports || 0;
+        var scanDate = abuseData.data.scanDate || 'N/A';
+        var scanResults = abuseData.data.scanResults || 'N/A';
+        var country = abuseData.data.country || 'N/A';
+
+        abuseInfoElement.innerHTML = `VirusTotal Information:`;
+        abuseInfoElement.innerHTML += `<br>Abuse Confidence Score: ${abuseConfidenceScore}`;
+        abuseInfoElement.innerHTML += `<br>Suspicious Confidence Score: ${suspiciousConfidenceScore}`;
+        abuseInfoElement.innerHTML += `<br>Number of Reports: ${numReports}`;
+        abuseInfoElement.innerHTML += `<br>Last Analysis Date: ${scanDate}`;
+        abuseInfoElement.innerHTML += `<br>Last Analysis Results: ${scanResults}`;
+        abuseInfoElement.innerHTML += `<br>Country: ${country}`;
+    } else {
+        abuseInfoElement.innerHTML = `<br>No VirusTotal information available for this IP.`;
+    }
+}
